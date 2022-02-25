460d9980b7707875cde9d43c1784a972853d2bd2
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-02-25 09:25:34 +0700

    BT Thuc hanh 1 ( a -> e )

---
+++ BaiTap_TH/LTAndroid_TH_b1/app/src/main/java/com/example/ltandroid_th_b1/Activity_TH_D.java
@@ -0,0 +1,14 @@
+package com.example.ltandroid_th_b1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_TH_D extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_th_d);
+    }
+}
