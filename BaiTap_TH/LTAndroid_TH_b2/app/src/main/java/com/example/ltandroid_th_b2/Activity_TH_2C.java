797998b046408462976dab3ae727baa68fa7a323
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-02-25 10:47:48 +0700

    bài TH tuần 2 _ c

---
+++ BaiTap_TH/LTAndroid_TH_b2/app/src/main/java/com/example/ltandroid_th_b2/Activity_TH_2C.java
@@ -0,0 +1,26 @@
+package com.example.ltandroid_th_b2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity_TH_2C extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_th2_c);
+
+        TextView txtView = findViewById(R.id.txtView);
+        EditText edPass = findViewById(R.id.ed_Pass);
+        Button btnGenerate = findViewById(R.id.btnGenerate);
+
+
+
+
+
+    }
+}
