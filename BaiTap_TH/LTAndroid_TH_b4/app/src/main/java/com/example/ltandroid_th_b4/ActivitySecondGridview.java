2f40fe8df84b668019bea1882f51940960f52e9b
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-03-12 07:32:05 +0700

    BT TH Tuan 4 _ b

---
+++ BaiTap_TH/LTAndroid_TH_b4/app/src/main/java/com/example/ltandroid_th_b4/ActivitySecondGridview.java
@@ -0,0 +1,14 @@
+package com.example.ltandroid_th_b4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySecondGridview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_gridview);
+    }
+}
