d199e86e269485a6917cbcb7e65878ab441b1179
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-04-06 09:57:44 +0700

    update
    hoan thành (demo_b9_LT_Fragment_dsSP_TK3) + học SQLite

---
+++ demo_LT/demo_b8_LT_GridView_banSua/app/src/main/res/layout/code/Activity_CT_SanPham.java
@@ -0,0 +1,47 @@
+package com.example.demo_b8_lt_gridview_bansua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity_CT_SanPham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ct_san_pham);
+
+        ImageView imgAnh = findViewById(R.id.img_Ac3);
+        TextView txtName = findViewById(R.id.txtName_ac3);
+        TextView txtGia = findViewById(R.id.txtGia_ac3);
+        TextView txtXuatXu = findViewById(R.id.txtXuatXu_ac3);
+        TextView txtCongDung = findViewById(R.id.txtCongDung_ac3);
+        TextView txtThanhPhan = findViewById(R.id.txtThanhPhan_ac3);
+
+        SanPham a = (SanPham) getIntent().getSerializableExtra("sp");
+
+        imgAnh.setImageResource(a.getMaID());
+        txtName.setText(a.getTen());
+        txtXuatXu.setText(a.getXuatxu());
+        txtThanhPhan.setText("Thành phần: " + a.getThanhPhan());
+        txtCongDung.setText("Công dụng: " + a.getCongDung());
+        txtGia.setText(""+a.getGia()+" VND/Hộp( "+ a.getDungTich()+"ml )");
+
+
+
+
+        ImageView imgAdd = findViewById(R.id.imgAdd);
+        imgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Activity_CT_SanPham.this, "đang cập nhật.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
